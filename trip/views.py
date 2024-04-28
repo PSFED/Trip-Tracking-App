b8c049821e0a7540370093549f8fc9d1313e0e5c
Pavel Semenov <x.pavel.semenov.x@gmail.com>
2024-04-28 15:02:11 +0300

    initial commit

---
+++ trip/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView
+from .models import Trip, Note
+
+
+class HomeView(TemplateView):
+    template_name = 'trip/index.html'
+
+
+def trips_list(request):
+    trips = Trip.objects.filter(owner=request.user)
+    context = {
+        'trips': trips,
+    }
+    return render(request, 'trip/trip_list.html', context)
